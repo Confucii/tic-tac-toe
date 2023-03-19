cc91b71efee7c2ecc8e95cc2e1c407a1d7008685
Oleksii <lesha.avdeev@gmail.com>
2023-03-19 17:23:45 +0200

    Add win condition check

--- script/script.js
+++ script/script.js
@@ -10,13 +10,17 @@ const gameBoard = (() => {
   const _display = () => {
     const _boardTable = document.querySelectorAll("td");
     _boardTable.forEach((cell, index) => {
-      const cellRef = cell;
-      cellRef.textContent = _board[Math.floor(index / 3)][index % 3];
+      const _cellRef = cell;
+      _cellRef.textContent = _board[Math.floor(index / 3)][index % 3];
     });
   };
 
   const reset = () => {
-    _board = [[], [], []];
+    _board = [
+      ["", "", ""],
+      ["", "", ""],
+      ["", "", ""],
+    ];
   };
 
   const fillCell = function fillCell(_playerMarker) {
@@ -26,14 +30,87 @@ const gameBoard = (() => {
     }
   };
 
+  const checkWin = function checkWin(cell) {
+    let _overlapCount = 0;
+
+    const _checkOverlap = () => {
+      _overlapCount = _overlapCount === 2 ? true : 0;
+    };
+
+    const _checkHorizontal = () => {
+      for (let i = 0; i < 2; i += 1) {
+        if (_board[cell.dataset.row][i] === _board[cell.dataset.row][i + 1]) {
+          _overlapCount += 1;
+        }
+      }
+    };
+
+    const _checkVertical = () => {
+      for (let i = 0; i < 2; i += 1) {
+        if (
+          _board[i][cell.dataset.column] === _board[i + 1][cell.dataset.column]
+        ) {
+          _overlapCount += 1;
+        }
+      }
+    };
+
+    const _checkLeftToRightDiag = () => {
+      for (let i = 0; i < 2; i += 1) {
+        if (_board[i][i] === _board[i + 1][i + 1]) {
+          _overlapCount += 1;
+        }
+      }
+    };
+
+    const _checkRightToLeftDiag = () => {
+      for (let i = 0, j = 2; i < 2; i += 1, j -= 1) {
+        if (_board[i][j] === _board[i + 1][j - 1]) {
+          _overlapCount += 1;
+        }
+      }
+    };
+
+    _checkHorizontal();
+    _checkOverlap();
+
+    if (!_overlapCount) {
+      _checkVertical();
+      _checkOverlap();
+
+      if (!_overlapCount) {
+        if (cell.dataset.row === "1" && cell.dataset.column === "1") {
+          _checkLeftToRightDiag();
+          _checkOverlap();
+          if (!_overlapCount) {
+            _checkRightToLeftDiag();
+            _checkOverlap();
+          }
+        } else if (cell.dataset.row === cell.dataset.column) {
+          _checkLeftToRightDiag();
+          _checkOverlap();
+        } else {
+          _checkRightToLeftDiag();
+          _checkOverlap();
+        }
+      }
+    }
+    if (_overlapCount) {
+      return true;
+    }
+    return false;
+  };
+
   return {
     fillCell,
     reset,
+    checkWin,
   };
 })();
 
 const gameFlow = (() => {
   let _playerMarker = "";
+  let turn = 1;
 
   const initPlayers = () => {
     const playerOne = playerFactory("Alex", "X");
@@ -65,8 +142,15 @@ const gameFlow = (() => {
 
   __boardTable.forEach((cell) => {
     cell.addEventListener("click", () => {
-      gameBoard.fillCell.call(cell, _playerMarker);
-      _changePlayer();
+      if (turn < 10) {
+        gameBoard.fillCell.call(cell, _playerMarker);
+        _changePlayer();
+        turn += 1;
+      }
+
+      if (turn > 5) {
+        console.log(gameBoard.checkWin(cell));
+      }
     });
   });
 
